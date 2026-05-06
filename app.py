6211a8748867883e2c46e783b8d7929b0befdbe6
Sairaj-Serigara <sairajsherigar5@gmail.com>
2026-05-06 17:11:37 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    result = None
+    if request.method == "POST":
+        num1 = float(request.form["num1"])
+        num2 = float(request.form["num2"])
+        op = request.form["operation"]
+
+        if op == "add":
+            result = num1 + num2
+        elif op == "sub":
+            result = num1 - num2
+        elif op == "mul":
+            result = num1 * num2
+        elif op == "div":
+            result = num1 / num2
+
+    return render_template("index.html", result=result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
